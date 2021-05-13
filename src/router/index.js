ed6c1536c49fb2967c413404d2cd197742b164c7
thisLkk <m_lukuankuan@163.com>
2021-05-13 19:09:47 +0800

    feat: 配置history路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,9 @@
 import routesConfig from './route'
 import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/jintingyo-web/'),
   routes: routesConfig
 })
-
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.auth)) {
     next()
